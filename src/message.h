0acd8c1eeb2ad4b044f9447f84139f0b6d7f4ce8
Ulf Lehnert <u.lehnert@hzdr.de>
2022-03-02 16:54:57 +0100

    Revert "feature branch for refactoring the message class"
    This reverts commit 148f41fd860b5b0e7f52d7e9498add25b2d678c7.
    Want to do a feature branch istead, to keep linux_sim intact.

--- src/message.h
+++ src/message.h
@@ -8,144 +8,166 @@
     The modules shall be as independent as possible, so, usually they don't
     know the type of module they are communicating with. The ports are wired
     at runtime during system creation when all modules are created.
-
-    All messages contain a sender information, a signature byte of the
-    message type and a type-specific data block. This way in principle
-    all ports can send and receive arbitrary types of messages.
-    The date block is preceeded by its size in bytes and usually
-    contains a strucure. The most common message types are defined here
-    but modules could well define more types.
-    Receiver ports may drop messages which are of not-handled types.
+    
+    The messages are typed, so a compile-time check of the compatibility
+    of sender and receiver ports is possible. Every module can define it's
+    own message types. This, however, required that modules import the
+    headers of the other modules they want to communicate with.
+    To avoid that, many message types are predefined.
+    Modules can use those without knowing the sender/receiver in advance.
 */
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <string>
 
-// message type IDs
-# define MSG_TYPE_TEXT              1
-# define MSG_TYPE_SYSTEM            2
-# define MSG_TYPE_TELEMETRY         3
-# define MSG_TYPE_GPS_POSITION     10
-# define MSG_TYPE_IMU_GYRO         20
-# define MSG_TYPE_IMU_ACCEL        21
-# define MSG_TYPE_IMU_MAG          22
-# define MSG_TYPE_IMU_AHRS         23
+#define MSG_LEVEL_FATALERROR 1
+#define MSG_LEVEL_CRITICAL 3
+#define MSG_LEVEL_MILESTONE 5
+#define MSG_LEVEL_ERROR 8
+#define MSG_LEVEL_STATE_CHANGE 10
+#define MSG_LEVEL_WARNING 12
+#define MSG_LEVEL_IMPORTANTTELEMETRY 20
+#define MSG_LEVEL_STATUSREPORT 30
+#define MSG_LEVEL_TELEMETRY 50
 
-// ---------------------------------------------
-// data structure of the different message types
-// ---------------------------------------------
-
-// general text message (may be another message serialized)
-struct  MSG_TEXT {
-    std::string text;
+/*
+    This is the base class for all messages.
+    It defines all functionality a message must provide.
+    It cannot be used by itself, only derived classes that implement the defined functionality.
+*/
+class Message {
+    public:
+        // standard constructor
+        Message(std::string sender_module);
+        
+        // Generate a string with a standardized format holding the message.
+        // There is no CR/LF at the end of the string, a print routine has to add that if necessary.
+        // this must be overridden by derived classes
+        virtual std::string serialize() = 0;
+        
+        // Generate a text message with all information but the sender id serialized
+        // this must be overridden by derived classes
+        // we cannot declare it here because the derived class is still abstract
+        // virtual Message_Text as_text() = 0;
+        
+        // there is one single member that is required for all messages
+        // the sender module of the message
+        std::string m_sender_module;
 };
 
-struct  MSG_SYSTEM {
-    uint32_t time;
-    uint8_t severity_level;
-    std::string text;
+/*
+    These are general text messages.
+*/
+class Message_Text : public Message {
+    public:
+        // constructor
+        Message_Text(
+            std::string sender_module,
+            std::string text);
+        // override the serialization function
+        virtual std::string serialize();
+        virtual Message_Text as_text();
+        // additional members
+        std::string m_text;
 };
 
-struct  MSG_TELEMETRY {
-    uint32_t time;
-    std::string variable;
-    std::string value;
+/*
+    These are system messages going to a logfile or to the downlink.
+    They have an indication of how important the message is
+    and at which time the message was sent.
+    time : milliseconds since system start will be serialized as seconds with 1 ms resolution
+*/
+class Message_System : public Message {
+    public:
+        // constructor
+        Message_System(
+            std::string sender_module,
+            uint32_t time,
+            uint8_t severity_level,
+            std::string text);
+        // override the serialization function
+        virtual std::string serialize();
+        virtual Message_Text as_text();
+        // additional members
+        uint8_t m_severity_level;
+        uint32_t m_time;
+        std::string m_text;
 };
 
-struct  MSG_GPS_POSITION {
-    double  latitude;       // degree north
-    double  longitude;      // degree east
-    float   altitude;       // meters above MSL
+/*
+    These are telemetry messages for automatted evaluation.
+    They contain exactly one data item (variable) of given name.
+    they contain the time the message was sent.
+    time : milliseconds since system start will be serialized as seconds with 1 ms resolution
+*/
+class Message_Telemetry : public Message {
+    public:
+        // constructor
+        Message_Telemetry(
+            std::string sender_id,
+            uint32_t time,
+            std::string variable,
+            std::string value);
+        // override the serialization function
+        virtual std::string serialize();
+        virtual Message_Text as_text();
+        // additional members
+        uint32_t m_time;
+        std::string m_variable;
+        std::string m_value;
+};
+
+/*
+    These are messages sent by a GPS module containing the current position
+*/
+class Message_GPS_position : public Message {
+    public:
+        // constructor
+        Message_GPS_position(
+            std::string sender_id,
+            double  latitude,       // degree north
+            double  longitude,      // degree east
+            float   altitude);      // meters above MSL
+        // override the serialization function
+        virtual std::string serialize();
+        virtual Message_Text as_text();
+        // additional members
+        double  m_latitude;
+        double  m_longitude;
+        float   m_altitude;
 };
 
 // in airframe-fixed coordinates (right, forward, up)
-struct MSG_IMU_GYRO {
+struct MESSAGE_IMU_GYRO {
     float   nick;           // rate [deg/s] positive up
     float   roll;           // rate [deg/s] positive left
     float   yaw;            // rate [deg/s] positive left
 };
 
 // in airframe-fixed coordinates (right, forward, up)
-struct MSG_IMU_ACCEL {
+struct MESSAGE_IMU_ACCEL {
     float   right;          // acceleration [G]
     float   forward;        // acceleration [G]
     float   up;             // acceleration [G]
 };
 
 // in airframe-fixed coordinates (right, forward, up)
-struct MSG_IMU_MAG {
+struct MESSAGE_IMU_MAG {
     float   right;          // field Bx [Gs]
     float   forward;        // field By [Gs]
     float   up;             // field Bz [Gs]
 };
 
 // in earth-fixed coordinates
-struct MSG_IMU_AHRS {
+struct MESSAGE_IMU_AHRS {
     float   attitude;       // angle of attack with respect to horizontal flight [deg]
                             // range  -180 ... +180 deg, positive up (hover is +90 deg)
     float   heading;        // with respect to magnetic north [deg], range 0 ... 360 deg
                             // nose direction for attitude=-90..+90 deg, else tail direction
     float   roll;           // roll angle about heading (roll in horizontal flight, yaw in hover)
                             // range -90 ... 90 deg, positive left
-};
-
-// status messages have a severity level information
-#define MSG_LEVEL_FATALERROR            1
-#define MSG_LEVEL_CRITICAL              3
-#define MSG_LEVEL_MILESTONE             5
-#define MSG_LEVEL_ERROR                 8
-#define MSG_LEVEL_STATE_CHANGE         10
-#define MSG_LEVEL_WARNING              12
-#define MSG_LEVEL_IMPORTANTTELEMETRY   20
-#define MSG_LEVEL_STATUSREPORT         30
-#define MSG_LEVEL_TELEMETRY            50
-
-
-class Message {
-    public:
-        // standard constructor
-        // the data block is copied into newly allocated memory
-        Message(
-            std::string sender_module,
-            uint8_t msg_type,
-            uint16_t data_size,
-            unsigned char *data
-            );
-        
-        // copy constructor
-        // create a clone of the given message
-        Message(const Message& msg);
-        
-        // destructor
-        ~Message();
-
-        // generate a possibly compact array of bytes for transmission over
-        // bandwidth-limited communication channels
-        std::string byte_string();
-                
-        // Generate a string with a standardized format holding the message.
-        // There is no CR/LF at the end of the string, a print routine has to add that if necessary.
-        std::string print();
-        
-        // Generate a text message with all information but the sender id serialized
-        Message as_text();
-        
-    private:   
-        // the sender module of the message
-        std::string sender_module;
-        
-        // the message type
-        uint8_t msg_type;
-        
-        // the sie of the data structure
-        uint16_t data_size;
-        
-        // pointer to the data block
-        // the data block will be allocated/freed with the message
-        // upon creation it is filled from the provided memory location
-        // when data_size==0 this pointer is NULL - do not access
-        unsigned char *data;
 };
 
