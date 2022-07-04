0023d8007d09279d23291a50eccede436561e47e
Ulf Lehnert <ulf@lehnertu.de>
2022-07-04 14:52:34 +0200

    altitude display for ground control station

--- ground_control_station/lib/ui/pfd.dart
+++ ground_control_station/lib/ui/pfd.dart
@@ -86,11 +86,17 @@ class StatePFD extends State<PFD> {
             ],
           ),
         ),
-        Expanded(
+        Flexible(
           flex: 1,
           // here goes the altitude display
           child: Container(
             color: const Color.fromARGB(255, 168, 168, 168),
+            constraints: BoxConstraints(
+              minWidth: 150,
+              maxWidth: double.infinity,
+              minHeight: 500,
+              maxHeight: double.infinity,
+            ),
             child: CustomPaint(
               painter: AltitudePainter(_altimeter),
             ),
@@ -107,11 +113,11 @@ class AltitudePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final double leftEdge = (size.width * 0.5).round().toDouble();
+    final double leftEdge = (size.width * 0.6).round().toDouble();
     final double rightEdge = (leftEdge + 30).toDouble();
     final double bottomEdge = (0.9 * size.height).toDouble();
     final double topEdge = 0.1 * size.height.toDouble();
-    final double pixelsPerMeter = (bottomEdge-topEdge) / 50.0;
+    final double pixelsPerMeter = (bottomEdge - topEdge) / 50.0;
     final double centerPixel = size.height * 0.5;
 
     // pixel scale of altitude centered about alt
@@ -136,31 +142,28 @@ class AltitudePainter extends CustomPainter {
         ..strokeWidth = 0
         ..color = const Color.fromARGB(255, 208, 150, 57);
       canvas.drawRect(
-          Rect.fromLTWH(
-              leftEdge, gBoxTop, 30, 0.9 * size.height - gBoxTop),
+          Rect.fromLTWH(leftEdge, gBoxTop, 30, 0.9 * size.height - gBoxTop),
           groundBox);
     }
     final yBoxBottom = scale(0.0) < bottomEdge ? scale(0.0) : bottomEdge;
     final yBoxTop = scale(20.0) > topEdge ? scale(20.0) : topEdge;
-    if ((yBoxTop<bottomEdge) & (yBoxBottom>topEdge)) {
+    if ((yBoxTop < bottomEdge) & (yBoxBottom > topEdge)) {
       final groundBox = Paint()
         ..style = PaintingStyle.fill
         ..strokeWidth = 0
         ..color = const Color.fromARGB(255, 253, 255, 124);
       canvas.drawRect(
-          Rect.fromLTWH(
-              leftEdge, yBoxTop, 30,yBoxBottom - yBoxTop),
+          Rect.fromLTWH(leftEdge, yBoxTop, 30, yBoxBottom - yBoxTop),
           groundBox);
     }
     final bBoxBottom = scale(20.0) < bottomEdge ? scale(20.0) : bottomEdge;
-    if (bBoxBottom>topEdge) {
+    if (bBoxBottom > topEdge) {
       final groundBox = Paint()
         ..style = PaintingStyle.fill
         ..strokeWidth = 0
         ..color = const Color.fromARGB(255, 196, 244, 255);
       canvas.drawRect(
-          Rect.fromLTWH(
-              leftEdge, topEdge, 30,bBoxBottom - topEdge),
+          Rect.fromLTWH(leftEdge, topEdge, 30, bBoxBottom - topEdge),
           groundBox);
     }
     // outline of the scale bar
@@ -177,14 +180,14 @@ class AltitudePainter extends CustomPainter {
         // label the markers
         TextSpan ts = TextSpan(
           text: m.toStringAsFixed(0),
-          style: const TextStyle(color: Colors.black, fontSize: 16),
+          style: const TextStyle(color: Colors.black, fontSize: 14),
         );
         TextPainter tp = TextPainter(
             text: ts,
             textAlign: TextAlign.right,
             textDirection: TextDirection.ltr);
         tp.layout(minWidth: 40.0, maxWidth: 60);
-        tp.paint(canvas, Offset(leftEdge - 50, scale(m) - 10));
+        tp.paint(canvas, Offset(leftEdge - 50, scale(m) - 9));
       } else if (m % 5 == 0) {
         canvas.drawLine(
           Offset(leftEdge + 5, scale(m)),
@@ -206,26 +209,26 @@ class AltitudePainter extends CustomPainter {
       ..strokeWidth = 2.0
       ..color = Colors.white;
     canvas.drawRect(
-        Rect.fromLTWH(leftEdge - 81, centerPixel - 20, 80, 40), altBox);
+        Rect.fromLTWH(leftEdge - 71, centerPixel - 16, 70, 32), altBox);
     final altOutline = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 5.0
       ..color = Colors.black;
     canvas.drawRect(
-        Rect.fromLTWH(leftEdge - 81, centerPixel - 20, 80, 40), altOutline);
+        Rect.fromLTWH(leftEdge - 71, centerPixel - 16, 70, 32), altOutline);
     TextSpan ts = TextSpan(
       text: alt.toStringAsFixed(0),
       style: const TextStyle(
         color: Colors.black,
-        fontSize: 30,
+        fontSize: 24,
         fontFamily: 'Monospace',
         fontWeight: FontWeight.bold,
       ),
     );
     TextPainter tp = TextPainter(
         text: ts, textAlign: TextAlign.right, textDirection: TextDirection.ltr);
-    tp.layout(minWidth: 70.0, maxWidth: 70);
-    tp.paint(canvas, Offset(leftEdge - 75, centerPixel - 20));
+    tp.layout(minWidth: 65.0, maxWidth: 65);
+    tp.paint(canvas, Offset(leftEdge - 71, centerPixel - 14));
   }
 
   @override
